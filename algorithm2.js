374ef325fe3cac2b45b009b7e08bdd5828c642aa
MucuriAmalia <aronmucuri@gmail.com>
2024-10-14 12:14:06 +0300

    Add files via upload

---
+++ algorithm2.js
@@ -0,0 +1,19 @@
+function insertionSort(arr) {
+    let n = arr.length;
+    for (let i = 1; i < n; i++) {
+        let current = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j]; 
+            j--;
+        }
+        arr[j + 1] = current;
+    }
+    return arr;
+}
+// Test
+let arr = [15, 7, 20, 9, 4];
+console.log("Original array:", arr);
+
+let sortedArr = insertionSort(arr);
+console.log("Sorted array:", sortedArr);
